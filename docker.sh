44b2f02dd77a9a46c0be1dc84a124dffcce01584
Pete Moore <pmoore@mozilla.com>
2020-08-24 22:01:46 +0200

    Added comments to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# This script builds everything under docker.
+# To see how the docker image was created, see the docker subdirectory.
+
 cd "$(dirname "${0}")"
 TAG="$(cat docker/TAG)"
 docker image inspect "${TAG}" >/dev/null 2>&1 || docker pull "${TAG}"

f9d514e3ee5e32465e09b30cef1ee2050124a2c5
Pete Moore <pmoore@mozilla.com>
2020-11-15 13:49:32 +0100

    Removed `docker pull` since `docker run` already pulls if needed

--- docker.sh
+++ docker.sh
@@ -5,5 +5,4 @@
 
 cd "$(dirname "${0}")"
 TAG="$(cat docker/TAG)"
-docker image inspect "${TAG}" >/dev/null 2>&1 || docker pull "${TAG}"
 docker run --rm -v "$(pwd):/spectrum4" "${TAG}" /spectrum4/all.sh

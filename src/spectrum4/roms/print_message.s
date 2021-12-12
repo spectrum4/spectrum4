975b041fd74e090470e4ea43807e7e64f07f67ed
Pete Moore <pmoore@mozilla.com>
2021-12-12 13:58:33 +0100

    wip

--- src/spectrum4/roms/print_message.s
+++ src/spectrum4/roms/print_message.s
@@ -2,19 +2,15 @@
 .align 2
 # Print zero byte delimited string stored at memory location x0 to current channel.
 # On entry:
-#   x0 = address of zero byte delimited string
+#   x2 = address of zero byte delimited string
 print_message:                           // L057D
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  stp     x19, x20, [sp, #-16]!                   // Backup x19 / x20 on stack.
-  mov     x19, x0
   b       2f
-1:
-  mov     w0, w1
-  bl      print_w0
-2:
-  ldrb    w1, [x19], #1
-  cbnz    w1, 1b
-  ldp     x19, x20, [sp], #0x10                   // Restore old x19, x20.
+  1:
+    bl      print_w0
+  2:
+    ldrb    w0, [x2], #1
+    cbnz    w0, 1b
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

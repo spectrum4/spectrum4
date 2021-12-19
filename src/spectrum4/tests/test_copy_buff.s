5895b35412155907e1a8280a9ce000a70e26a52d
Pete Moore <pmoore@mozilla.com>
2021-12-19 09:15:39 +0100

    No spectrum4 tests for copy_buff

---
+++ src/spectrum4/tests/test_copy_buff.s
@@ -0,0 +1,21 @@
+# This file is part of the Spectrum +4 Project.
+# Licencing information can be found in the LICENCE file
+# (C) 2021 Spectrum +4 Authors. All rights reserved.
+
+
+.if ROMS_INCLUDE
+.else
+  .include "print_w0.s"
+.endif
+
+
+# TODO: implement tests, once copy_buff is implemented. Note, copy_buff is
+# routine that writes printer buffer to printer, and we don't have a printer
+# interface on spectrum4 yet (could potentially write binary data to UART, and
+# have software on receiving end to convert to an image).
+
+
+.text
+.align 2
+copy_buff_1_setup_regs:
+  ret

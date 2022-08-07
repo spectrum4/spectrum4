3fb5ce5f12d03a887954ede6e28d1cff3b4483a3
Pete Moore <pmoore@mozilla.com>
2022-08-07 11:57:09 +0200

    macro logreg fix:
    * don't require that tests are included (base10 routine moved from runtests to kernel)
    * preserve nzcv flags in x5 rather than x4 since base10 subroutine disturbs x4

--- src/spectrum4/kernel/kernel.s
+++ src/spectrum4/kernel/kernel.s
@@ -138,7 +138,7 @@
   stp     x0, x1, [sp, #-16]!
   stp     x2, x3, [sp, #-16]!
   stp     x4, x5, [sp, #-16]!
-  mrs     x4, nzcv                                // copy N, Z, C, and V flags into x4 (not disturbed by following uart_puts call)
+  mrs     x5, nzcv                                // copy N, Z, C, and V flags into x5 (not disturbed by following uart_puts, uart_send, uart_x0, uart_newline, base10 calls)
   mov     x0, 'x'
   bl      uart_send
   mov     x2, #\index
@@ -157,7 +157,7 @@
   mov     x0, x\index
   bl      uart_x0
   bl      uart_newline
-  msr     nzcv, x4                                // restore flags
+  msr     nzcv, x5                                // restore flags
   ldp     x4, x5, [sp], #16
   ldp     x2, x3, [sp], #16
   ldp     x0, x1, [sp], #16
@@ -889,5 +889,37 @@ uart_x0:
   ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
   ret
 .endif
+
+
+# Converts a uint64 value to ASCII decimal representation. The caller must
+# provide a buffer of at least 21 bytes. The result will be written to the end of
+# the buffer, including a trailing 0 byte. The start address will be returned.
+# The generated decimal string has no left padding with 0's or spaces etc.
+#
+# On entry:
+#   x0 = address of 21 (or more) byte buffer to write to
+#   x2 = value to convert to decimal
+# On exit:
+#   x0 = address of start of string within buffer
+#   x1 = first char (byte) of generated string (e.g. 48-57 for '0'-'9', _not_ the address)
+#   x2 = 0
+#   x3 = 0xcccccccccccccccd
+#   x4 = 0
+base10:
+  add     x0, x0, #0x14                           // x0 = address of last allocated byte
+  strb    wzr, [x0]                               // Store 0 in last allocated byte
+  mov     x3, #0xcccccccccccccccc
+  add     x3, x3, #1                              // x3 = constant for divison by 10
+  1:
+    umulh   x4, x3, x2
+    lsr     x4, x4, #3                            // x4 = x2 / 10
+    add     x1, x4, x4, lsl #2                    // x1 = 5 * x4
+    sub     x2, x2, x1, lsl #1                    // x2 = x2 % 10 = value of last digit
+    add     x1, x2, #0x30                         // x1 = ASCII value of digit = x2 + 48
+    strb    w1, [x0, #-1]!                        // store ASCII value on stack at correct offset
+    mov     x2, x4                                // x2 = previous x2 / 10
+    cbnz    x2, 1b                                // if x2 != 0 continue looping
+3:
+  ret
 
 .include "font.s"

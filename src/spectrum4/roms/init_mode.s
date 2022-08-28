de3aeb8845f017a433f9456cd37348c91989b45a
Pete Moore <pmoore@mozilla.com>
2022-08-28 16:30:57 +0200

    wip

--- src/spectrum4/roms/init_mode.s
+++ src/spectrum4/roms/init_mode.s
@@ -3,4 +3,9 @@
 # On entry:
 # On exit:
 init_mode:                               // L2584
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  bl      reset_cursor
+  strh    wzr, [x28, FC9A-sysvars]
+  ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

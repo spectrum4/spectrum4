e108d2b0401f739d5e6fc50f1371d05450fefa3e
Pete Moore <pmoore@mozilla.com>
2022-08-28 16:57:04 +0200

    wip

--- src/spectrum4/roms/reset_indentation.s
+++ src/spectrum4/roms/reset_indentation.s
@@ -2,15 +2,8 @@
 .align 2
 # On entry:
 # On exit:
-init_mode:                               // L2584
+reset_indentation:                                // L?
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  bl      reset_cursor
-  strh    wzr, [x28, FC9A-sysvars]
-  mov     w0, #0x82
-  strb    w0, [x28, EC0D-sysvars]                 // Waiting for key press (bit 7), menu displayed (bit 1)
-  strh    wzr, [x28, E_PPC-sysvars]
-  bl      reset_indentation
-  bl      mode_l
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

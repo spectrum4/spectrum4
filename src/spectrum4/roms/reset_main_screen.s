8ad2cecece1abcb9f38cd82bee7c504bd0ca1e28
Pete Moore <pmoore@mozilla.com>
2022-09-03 10:45:22 +0200

    wip

--- src/spectrum4/roms/reset_main_screen.s
+++ src/spectrum4/roms/reset_main_screen.s
@@ -2,15 +2,12 @@
 .align 2
 
 # ---------------------
-# Reset Cursor Position
 # ---------------------
 #
 # On entry:
 # On exit:
-reset_cursor:                            // L28BE
+reset_main_screen:                       // L2E1F
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  bl      reset_main_screen
-  bl      init_cursor
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

736be18b79f7af899079c56dc0869808cf3419bb
Pete Moore <pmoore@mozilla.com>
2022-09-04 09:04:38 +0200

    wip

--- src/spectrum4/roms/init_cursor.s
+++ src/spectrum4/roms/init_cursor.s
@@ -9,5 +9,8 @@
 init_cursor:                             // L3A7F
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
+  mov     x2, #0x000f000000000000
+  add     x2, x2, #0x0000000000140000
+  str     x2, [x28, FD6C-sysvars]
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

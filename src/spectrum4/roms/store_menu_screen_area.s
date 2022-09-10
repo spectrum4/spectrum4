cd4e1d01491c340aa37715a8b4ddc9dea0e9cc50
Pete Moore <pmoore@mozilla.com>
2022-09-10 10:03:25 +0200

    wip

--- src/spectrum4/roms/store_menu_screen_area.s
+++ src/spectrum4/roms/store_menu_screen_area.s
@@ -2,17 +2,12 @@
 .align 2
 
 # ------------------------
-# Display Menu
 # ------------------------
 #
 # On entry:
 # On exit:
-display_menu:                            // L36A8
+store_menu_screen_area:                  // L373B
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  bl      store_menu_screen_area
-  ldrb    w0, [x28, TV_FLAG-sysvars]
-  and     w0, w0, #0xfe
-  strb    w0, [x28, TV_FLAG-sysvars]              // Clear bit 0 of TV_FLAG: main screen in use
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret

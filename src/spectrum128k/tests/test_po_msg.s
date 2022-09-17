5f1adbb34d3ce47c96486e7e73bf08cd227d6107
Pete Moore <pmoore@mozilla.com>
2022-09-17 16:54:16 +0200

    Add tests for control characters 0x10 (INK) and 0x11 (PAPER)

--- src/spectrum128k/tests/test_po_msg.s
+++ src/spectrum128k/tests/test_po_msg.s
@@ -164,3 +164,135 @@ po_msg_03_effects_regs:
 
 msg_po_msg_03_out:
   .asciz "33"
+
+
+# This test checks that if we print a string which contains the control code
+# sequence 0x10, 0x03, 0x11, 0x02, 0x11, 0x09, 0x10, 0x09 (INK 3, PAPER 2,
+# PAPER 9, INK 9) that we end up with ATTR_T holding INK 0 and PAPER 7.
+#
+# Note this is different to what we get if we swap the order of the control
+# codes from PAPER 9, INK 9 to INK 9, PAPER 9 which is demonstrated in the
+# subsequent test.
+po_msg_paper_ink_9_1_setup:
+  _strh   sysvars_48k_end, CHANS          ; channel information table stored after sysvars
+  _strh   sysvars_48k_end, CURCHL         ; current stream is -3 (channel 'K')
+  ld      hl, init_strm
+  ld      de, STRMS
+  ld      bc, 0x000e
+  ldir                                    ; default stream information table
+  ld      hl, init_chan
+  ld      de, sysvars_48k_end
+  ld      bc, 0x0015
+  ldir                                    ; default channel information table
+  ret
+
+po_msg_paper_ink_9_1_setup_regs:
+  xor     a
+  ld      de, po_msg_paper_ink_9_1_text
+  ld      l, 0x1b                         ; E holds value of entry L on exit, so give it a value so we can check E matches
+  ret
+
+po_msg_paper_ink_9_1_effects_regs:
+  ldf     0x87
+  ld      a, 0x12
+  ld      bc, 0x1000
+  ld      d, 0
+  ld      e, 0x1b
+  ret
+
+po_msg_paper_ink_9_1_effects:
+  _strb   0x10, TVDATA                    ; last control code in string (INK)
+
+  _resbit 0, ATTR_T
+  _resbit 1, ATTR_T
+  _resbit 2, ATTR_T                       ; ink 0
+
+  _setbit 3, ATTR_T
+  _setbit 4, ATTR_T
+  _setbit 5, ATTR_T                       ; paper 7
+
+  _setbit 0, MASK_T
+  _setbit 1, MASK_T
+  _setbit 2, MASK_T                       ; ink colour to be taken from ATTR_T
+
+  _setbit 3, MASK_T
+  _setbit 4, MASK_T
+  _setbit 5, MASK_T                       ; paper colour to be taken from ATTR_T
+
+  _setbit 4, P_FLAG                       ; ink 9
+  _setbit 6, P_FLAG                       ; paper 9
+  ret
+
+po_msg_paper_ink_9_1_text:
+  # any byte value with bit 7 set here is fine
+  .byte   0xab
+  .byte   0x10, 0x03                      ; ink 3
+  .byte   0x11, 0x02                      ; paper 2
+  .byte   0x11, 0x09                      ; paper 9
+  .byte   0x10, 0x09 | 0x80               ; ink 9 (bit 7 set => end marker)
+
+
+# This test checks that if we print a string which contains the control code
+# sequence 0x10, 0x03, 0x11, 0x02, 0x10, 0x09, 0x11, 0x09 (INK 3, PAPER 2,
+# INK 9, PAPER 9) that we end up with ATTR_T holding INK 7 and PAPER 0.
+#
+# Note this is different to what we get if we swap the order of the control
+# codes from INK 9, PAPER 9 to PAPER 9, INK 9 which is demonstrated in the
+# previous test.
+po_msg_paper_ink_9_2_setup:
+  _strh   sysvars_48k_end, CHANS          ; channel information table stored after sysvars
+  _strh   sysvars_48k_end, CURCHL         ; current stream is -3 (channel 'K')
+  ld      hl, init_strm
+  ld      de, STRMS
+  ld      bc, 0x000e
+  ldir                                    ; default stream information table
+  ld      hl, init_chan
+  ld      de, sysvars_48k_end
+  ld      bc, 0x0015
+  ldir                                    ; default channel information table
+  ret
+
+po_msg_paper_ink_9_2_setup_regs:
+  xor     a
+  ld      de, po_msg_paper_ink_9_2_text
+  ld      l, 0x1c                         ; E holds value of entry L on exit, so give it a value so we can check E matches
+  ret
+
+po_msg_paper_ink_9_2_effects_regs:
+  ldf     0x87
+  ld      a, 0x12
+  ld      bc, 0x4000
+  ld      d, 0
+  ld      e, 0x1c                         ; entry L
+  ret
+
+po_msg_paper_ink_9_2_effects:
+  _strb   0x11, TVDATA                    ; last control code in string (PAPER)
+
+  _setbit 0, ATTR_T
+  _setbit 1, ATTR_T
+  _setbit 2, ATTR_T                       ; ink 7
+
+  _resbit 3, ATTR_T
+  _resbit 4, ATTR_T
+  _resbit 5, ATTR_T                       ; paper 0
+
+  _setbit 0, MASK_T
+  _setbit 1, MASK_T
+  _setbit 2, MASK_T                       ; ink colour to be taken from ATTR_T
+
+  _setbit 3, MASK_T
+  _setbit 4, MASK_T
+  _setbit 5, MASK_T                       ; paper colour to be taken from ATTR_T
+
+  _setbit 4, P_FLAG                       ; ink 9
+  _setbit 6, P_FLAG                       ; paper 9
+  ret
+
+po_msg_paper_ink_9_2_text:
+  # any byte value with bit 7 set here is fine
+  .byte   0xab
+  .byte   0x10, 0x03                      ; ink 3
+  .byte   0x11, 0x02                      ; paper 2
+  .byte   0x10, 0x09                      ; ink 9
+  .byte   0x11, 0x09 | 0x80               ; paper 9 (bit 7 set => end marker)

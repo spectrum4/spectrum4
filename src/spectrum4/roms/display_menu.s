d453ce9672508faf03cba4bfca9e532b72cd5f58
Pete Moore <pmoore@mozilla.com>
2022-09-18 15:36:25 +0200

    wip

--- src/spectrum4/roms/display_menu.s
+++ src/spectrum4/roms/display_menu.s
@@ -1,5 +1,4 @@
 .text
-.align 2
 
 # ------------------------
 # Display Menu
@@ -9,6 +8,7 @@
 #   x1 = jump table
 #   x7 = menu text
 # On exit:
+.align 2
 display_menu:                            // L36A8
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
@@ -22,17 +22,35 @@ display_menu:                            // L36A8
   mov     x2, x7
   bl      print_message
   bl      print_sinclair_stripes
+  adr     x4, menu_title_space
+  bl      print_str_ff                            // Print remainder of menu title
   1:
   b       1b
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret
 
 
+# ------------------------
+# Menu Title Colours Table
+# ------------------------
+.align 0
 menu_title_colours:                      // L37EC
-  .byte 0x16, 0x19, 0x2d                          // AT 25, 45;
+  .byte 0x16, 0x19, 0x2f                          // AT 25, 47;
   .byte 0x15, 0x00                                // OVER 0;
   .byte 0x14, 0x00                                // INVERSE 0;
   .byte 0x10, 0x07                                // INK 7;
   .byte 0x11, 0x00                                // PAPER 0;
   .byte 0x13, 0x01                                // BRIGHT 1;
+  .byte 0xff                                      // end marker
+
+
+# ----------------------
+# Menu Title Space Table
+# ----------------------
+.align 0
+menu_title_space:                        // L37FA
+  .byte 0x11, 0x00                                // PAPER 0
+  .byte ' '
+  .byte 0x11, 0x07                                // PAPER 7
+  .byte 0x10, 0x00                                // INK 0
   .byte 0xff                                      // end marker

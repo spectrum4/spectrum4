b7e0038a50f5e98bde3ea528693261fabfe031e9
Pete Moore <pmoore@mozilla.com>
2022-09-24 11:11:34 +0200

    wip

--- src/spectrum4/roms/display_menu.s
+++ src/spectrum4/roms/display_menu.s
@@ -5,8 +5,8 @@
 # ------------------------
 #
 # On entry:
-#   x1 = jump table
-#   x7 = menu text
+#   x19 = jump table
+#   x20 = menu text
 # On exit:
 .align 2
 display_menu:                            // L36A8
@@ -16,16 +16,40 @@ display_menu:                            // L36A8
   ldrb    w0, [x28, TV_FLAG-sysvars]
   and     w0, w0, #0xfe
   strb    w0, [x28, TV_FLAG-sysvars]              // Clear bit 0 of TV_FLAG: main screen in use
-  ldrb    w0, [x1], #8                            // w0 = number of entries
+  ldrb    w23, [x19], #8                          // w23 = number of entries
   adr     x4, menu_title_colours
   bl      print_str_ff                            // Print title colours and position
-  mov     x2, x7
+  mov     x2, x20
   bl      print_message
+  mov     x20, x2
   bl      print_sinclair_stripes
   adr     x4, menu_title_space
   bl      print_str_ff                            // Print remainder of menu title
+  mov     w8, 0x1a
   1:
-  b       1b
+    mov     w9, 0x2f
+    mov     w22, w8
+    bl      print_at
+    mov     w0, ' '
+    bl      print_w0
+    mov     w21, 0x0d                             // Number of chars to print (including space padding)
+  2:
+    ldrb    w0, [x20], #1
+    cbz     w0, 3f
+    bl      print_w0
+    sub     w21, w21, #1
+    b       2b
+  3:
+    4:
+      mov     w0, ' '
+      bl      print_w0
+      subs    w21, w21, #1
+      b.ne    4b
+    add     w8, w22, #1
+    subs    w23, w23, #1
+    b.ne    1b
+5:
+  b       5b
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret
 

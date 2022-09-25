40cd2a37f0199c5ce12bb5c1619c7a8194f1c19b
Pete Moore <pmoore@mozilla.com>
2022-09-25 16:51:14 +0200

    +4 menu border lines

---
+++ src/spectrum4/roms/plot_line.s
@@ -0,0 +1,27 @@
+.text
+
+# -------------------
+# -------------------
+# On entry:
+#   w19 = start y
+#   w20 = start x
+#   w21 = y delta for next pixel
+#   w22 = x delta for next pixel
+#   w23 = number of pixels
+# On exit:
+
+
+.align 2
+plot_line:                               // L3719
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  1:
+    mov     w12, w19
+    mov     w13, w20
+    bl      plot_sub_1
+    add     w19, w19, w21
+    add     w20, w20, w22
+    subs    w23, w23, #1
+    b.ne    1b
+  ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
+  ret

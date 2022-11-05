22c6564bc76fe03def74ba9f51eefe54de61cd99
Pete Moore <pmoore@mozilla.com>
2022-11-05 12:35:48 +0100

    Fix random number generation on rpi4/rpi400

---
+++ src/spectrum4/kernel/macros.s
@@ -0,0 +1,212 @@
+# This file is part of the Spectrum +4 Project.
+# Licencing information can be found in the LICENCE file
+# (C) 2021 Spectrum +4 Authors. All rights reserved.
+
+
+.macro _strb val, addr
+  mov     w0, \val & 0xff
+  adrp    x1, \addr
+  add     x1, x1, :lo12:\addr
+  strb    w0, [x1]
+.endm
+
+
+.macro _strh val, addr
+  mov     w0, \val & 0xffff
+  adrp    x1, \addr
+  add     x1, x1, :lo12:\addr
+  strh    w0, [x1]
+.endm
+
+
+# like _strh, but val in *big endian* format
+.macro _strhbe val, addr
+  mov     w0, ((\val & 0xff) << 8) | ((\val & 0xff00) >> 8)
+  adrp    x1, \addr
+  add     x1, x1, :lo12:\addr
+  strh    w0, [x1]
+.endm
+
+
+# like hword, but in *big endian* format (useful for linear pixel maps)
+.macro _hwordbe val
+  .hword   ((\val & 0xff) << 8) | ((\val & 0xff00) >> 8)
+.endm
+
+
+.macro _str val, addr
+  ldr     x0, =\val
+  adrp    x1, \addr
+  add     x1, x1, :lo12:\addr
+  str     x0, [x1]
+.endm
+
+
+.macro _pixel val, x, y
+  adrp    x2, framebuffer
+  add     x2, x2, :lo12:framebuffer
+  ldr     w0, [x2]
+  ldr     w1, [x2, pitch-framebuffer]
+  mov     w2, \y
+  umaddl  x0, w1, w2, x0
+  mov     w1, \x
+  movz    x2, \val & 0xffff
+  movk    x2, (\val >> 16) & 0xffff, lsl #16
+  movk    x2, (\val >> 32) & 0xffff, lsl #32
+  movk    x2, (\val >> 48) & 0xffff, lsl #48
+  add     x0, x0, x1, lsl #2
+  str     x2, [x0]
+.endm
+
+
+.macro _setmsk mask, address
+  ldrb    w0, [x28, \address-sysvars]
+  orr     w0, w0, \mask
+  strb    w0, [x28, \address-sysvars]
+.endm
+
+
+.macro _resmsk mask, address
+  ldrb    w0, [x28, \address-sysvars]
+  and     w0, w0, ~\mask
+  strb    w0, [x28, \address-sysvars]
+.endm
+
+
+.macro _setbit bit, address
+  _setmsk (1<<\bit), \address
+.endm
+
+
+.macro _resbit bit, address
+  _resmsk (1<<\bit), \address
+.endm
+
+
+# Load a 32-bit immediate using mov.
+.macro movl Wn, imm
+  movz    \Wn, \imm & 0xffff
+  movk    \Wn, (\imm >> 16) & 0xffff, lsl #16
+.endm
+
+
+.macro log char
+.if       UART_DEBUG
+  stp     x29, x30, [sp, #-16]!
+  stp     x0, x1, [sp, #-16]!
+  stp     x2, x3, [sp, #-16]!
+  stp     x4, x5, [sp, #-16]!
+  mrs     x4, nzcv                                // copy N, Z, C, and V flags into x4 (not disturbed by following uart_puts call)
+  mov     x0, #\char
+  bl      uart_send
+  bl      uart_newline
+  msr     nzcv, x4                                // restore flags
+  ldp     x4, x5, [sp], #16
+  ldp     x2, x3, [sp], #16
+  ldp     x0, x1, [sp], #16
+  ldp     x29, x30, [sp], #16
+.endif
+.endm
+
+
+.macro logreg index
+.if       UART_DEBUG
+  stp     x29, x30, [sp, #-16]!
+  stp     x0, x1, [sp, #-16]!
+  stp     x2, x3, [sp, #-16]!
+  stp     x4, x5, [sp, #-16]!
+  mrs     x0, nzcv
+  str     x0, [sp, #-16]!                         // preserve N, Z, C, and V flags on the stack
+  mov     x0, 'x'
+  bl      uart_send
+  mov     x2, #\index
+  sub     sp, sp, 32
+  mov     x0, sp
+  bl      base10
+  bl      uart_puts
+  add     sp, sp, #32
+  mov     x0, ':'
+  bl      uart_send
+  mov     x0, ' '
+  bl      uart_send
+  ldp     x4, x5, [sp, #16]                       // restore these registers, in case they are being logged!
+  ldp     x2, x3, [sp, #32]
+  ldp     x0, x1, [sp, #48]
+  mov     x0, x\index
+  bl      uart_x0
+  bl      uart_newline
+  ldr     x0, [sp], #16                           // fetch preserved nzcv from stack
+  msr     nzcv, x0                                // restore it
+  ldp     x4, x5, [sp], #16
+  ldp     x2, x3, [sp], #16
+  ldp     x0, x1, [sp], #16
+  ldp     x29, x30, [sp], #16
+.endif
+.endm
+
+
+.macro logregs
+  logreg  0
+  logreg  1
+  logreg  2
+  logreg  3
+  logreg  4
+  logreg  5
+  logreg  6
+  logreg  7
+  logreg  8
+  logreg  9
+  logreg  10
+  logreg  11
+  logreg  12
+  logreg  13
+  logreg  14
+  logreg  15
+  logreg  16
+  logreg  17
+  logreg  18
+  logreg  19
+  logreg  20
+  logreg  21
+  logreg  22
+  logreg  23
+  logreg  24
+  logreg  25
+  logreg  26
+  logreg  27
+  logreg  28
+  logreg  29
+  logreg  30
+  lognzcv
+.endm
+
+
+.macro lognzcv
+  logarm  NZCV
+.endm
+
+
+.macro clear_carry
+  stp     x1, x2, [sp, #-16]!                     // backup x1, x2
+  mrs     x1, nzcv                                // copy N, Z, C, and V flags into x1
+  mov     x2, #0x20000000                         // mask for the carry flag (bit 29)
+  bic     x1, x1, x2                              // clear the carry flag
+  msr     nzcv, x1                                // apply changes back to NZCV register
+  ldp     x1, x2, [sp], #16                       // restore x1, x2
+.endm
+
+
+.macro set_carry
+  stp     x1, x2, [sp, #-16]!                     // backup x1, x2
+  mrs     x1, nzcv                                // copy N, Z, C, and V flags into x1
+  mov     x2, #0x20000000                         // mask for the carry flag (bit 29)
+  orr     x1, x1, x2                              // set the carry flag
+  msr     nzcv, x1                                // apply changes back to NZCV register
+  ldp     x1, x2, [sp], #16                       // restore x1, x2
+.endm
+
+
+.macro nzcv value
+  tst     wzr, #1
+  ccmp    wzr, #0, \value, ne
+.endm
---
+++ src/spectrum4/kernel/uart.s
@@ -0,0 +1,237 @@
+# This file is part of the Spectrum +4 Project.
+# Licencing information can be found in the LICENCE file
+# (C) 2021 Spectrum +4 Authors. All rights reserved.
+
+# ------------------------------------------------------------------------------
+# See "BCM2837 ARM Peripherals" datasheet pages 8-19:
+#   https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
+# ------------------------------------------------------------------------------
+.set AUX_IRQ,        0x0000                       // Auxiliary Interrupt Status
+.set AUX_ENABLES,    0x0004                       // Auxiliary Enables
+.set AUX_MU_IO_REG,  0x0040                       // Mini Uart I/O Data
+.set AUX_MU_IER,     0x0044                       // Mini Uart Interrupt Enable
+.set AUX_MU_IIR,     0x0048                       // Mini Uart Interrupt Identify
+.set AUX_MU_LCR,     0x004C                       // Mini Uart Line Control
+.set AUX_MU_MCR,     0x0050                       // Mini Uart Modem Control
+.set AUX_MU_LSR,     0x0054                       // Mini Uart Line Status
+.set AUX_MU_MSR,     0x0058                       // Mini Uart Modem Status
+.set AUX_MU_SCRATCH, 0x005C                       // Mini Uart Scratch
+.set AUX_MU_CNTL,    0x0060                       // Mini Uart Extra Control
+.set AUX_MU_STAT,    0x0064                       // Mini Uart Extra Status
+.set AUX_MU_BAUD,    0x0068                       // Mini Uart Baudrate
+
+
+# ------------------------------------------------------------------------------
+# See "BCM2837 ARM Peripherals" datasheet pages 90-104:
+#   https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
+# ------------------------------------------------------------------------------
+.set GPFSEL1,        0x0004                       // GPIO Function Select 1
+.set GPPUD,          0x0094                       // GPIO Pin Pull-up/down Enable
+.set GPPUDCLK0,      0x0098                       // GPIO Pin Pull-up/down Enable Clock 0
+
+# ------------------------------------------------------------------------------
+# Initialise the Mini UART interface for logging over serial port.
+# Note, this is Broadcomm's own UART, not the ARM licenced UART interface.
+# ------------------------------------------------------------------------------
+.align 2
+uart_init:
+  adr     x4, mailbox_base                        // x4 = mailbox_base
+  ldr     x1, [x4, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+  ldr     w2, [x1, AUX_ENABLES]                   // w2 = [AUX_ENABLES] (Auxiliary enables)
+  orr     w2, w2, #1
+  str     w2, [x1, AUX_ENABLES]                   //   [AUX_ENABLES] |= 0x00000001 => Enable Mini UART.
+  str     wzr, [x1, AUX_MU_IER]                   //   [AUX_MU_IER_REG] = 0x00000000 => Disable Mini UART interrupts.
+  str     wzr, [x1, AUX_MU_CNTL]                  //   [AUX_MU_CNTL_REG] = 0x00000000 => Disable Mini UART Tx/Rx
+  mov     w2, #0x6                                // w2 = 6
+  str     w2, [x1, AUX_MU_IIR]                    //   [AUX_MU_IIR_REG] = 0x00000006 => Mini UART clear Tx, Rx FIFOs
+  mov     w3, #0x3                                // w3 = 3
+  str     w3, [x1, AUX_MU_LCR]                    //   [AUX_MU_LCR_REG] = 0x00000003 => Mini UART in 8-bit mode.
+  str     wzr, [x1, AUX_MU_MCR]                   //   [AUX_MU_MCR_REG] = 0x00000000 => Set UART1_RTS line high.
+  adr     x2, aux_mu_baud_reg
+  ldr     w2, [x2]
+  str     w2, [x1, AUX_MU_BAUD]                   //   [AUX_MU_BAUD_REG] = 0x0000010e (rpi3) or 0x0000021d (rpi4)
+                                                  //         => baudrate = system_clock_freq/(8*([AUX_MU_BAUD_REG]+1))
+                                                  //                       (as close to 115200 as possible)
+  ldr     x4, [x4, gpio_base-mailbox_base]        // x4 = [gpio_base] = 0x000000003f200000 (rpi3) or 0x00000000fe200000 (rpi4)
+  ldr     w2, [x4, GPFSEL1]                       // w2 = [GPFSEL1]
+  and     w2, w2, #0xfffc0fff                     // Unset bits 12, 13, 14 (FSEL14 => GPIO Pin 14 is an input).
+                                                  // Unset bits 15, 16, 17 (FSEL15 => GPIO Pin 15 is an input).
+  orr     w2, w2, #0x00002000                     // Set bit 13 (FSEL14 => GPIO Pin 14 takes alternative function 5).
+  orr     w2, w2, #0x00010000                     // Set bit 16 (FSEL15 => GPIO Pin 15 takes alternative function 5).
+  str     w2, [x4, GPFSEL1]                       //   [GPFSEL1] = updated value => Enable UART 1.
+  str     wzr, [x4, GPPUD]                        //   [GPPUD] = 0x00000000 => GPIO Pull up/down = OFF
+  mov     x5, #0x96                               // Wait 150 instruction cycles (as stipulated by datasheet).
+1:
+  subs    x5, x5, #0x1                            // x0 -= 1
+  b.ne    1b                                      // Repeat until x0 == 0.
+  mov     w2, #0xc000                             // w2 = 2^14 + 2^15
+  str     w2, [x4, GPPUDCLK0]                     //   [GPPUDCLK0] = 0x0000c000 => Control signal to lines 14, 15.
+  mov     x0, #0x96                               // Wait 150 instruction cycles (as stipulated by datasheet).
+2:
+  subs    x0, x0, #0x1                            // x0 -= 1
+  b.ne    2b                                      // Repeat until x0 == 0.
+  str     wzr, [x4, GPPUDCLK0]                    //   [GPPUDCLK0] = 0x00000000 => Remove control signal to lines 14, 15.
+  str     w3, [x1, AUX_MU_CNTL]                   //   [AUX_MU_CNTL_REG] = 0x00000003 => Enable Mini UART Tx/Rx
+.if       TESTS_INCLUDE
+  adrp    x0, uart_disable
+  add     x0, x0, :lo12:uart_disable
+  strb    wzr, [x0]
+.endif
+  ret                                             // Return.
+
+
+# ------------------------------------------------------------------------------
+# Send a byte over Mini UART
+# ------------------------------------------------------------------------------
+# On entry:
+#   x0: char to send
+# On exit:
+#   x1: [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+#   x2: Last read of [AUX_MU_LSR_REG] when waiting for bit 5 to be set
+uart_send:
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+1:
+  ldr     w2, [x1, AUX_MU_LSR]                    // w2 = [AUX_MU_LSR_REG]
+  tbz     x2, #5, 1b                              // Repeat last statement until bit 5 is set.
+
+/////////////////////
+// This following section allows us to disable UART output during testing but
+// setting the one byte test system variable 'uart_disable' to a non zero value
+// without affecting any register values so to not impact tests.
+.if       TESTS_INCLUDE
+  adrp    x1, uart_disable
+  add     x1, x1, :lo12:uart_disable
+  ldrb    w1, [x1]
+  cbnz    w1, 2f
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+  b       3f
+2:
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+  ret
+3:
+.endif
+/////////////////////
+  strb    w0, [x1, AUX_MU_IO_REG]                 //   [AUX_MU_IO_REG] = w0
+  ret
+
+.if       UART_DEBUG
+# ------------------------------------------------------------------------------
+# Send '\r\n' over Mini UART
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   <nothing>
+# On exit:
+#   x0: 0x0a
+#   x1: [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+#   x2: Last read of [AUX_MU_LSR_REG] when waiting for bit 5 to be set
+uart_newline:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  mov     x0, #13
+  bl      uart_send
+  mov     x0, #10
+  bl      uart_send
+  ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
+  ret
+
+
+# ------------------------------------------------------------------------------
+# Send a null terminated string over Mini UART.
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   x0 = address of null terminated string
+# On exit:
+#   x0 = address of null terminator
+#   x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+#   x2 = 0
+#   x3 = [AUX_MU_LSR]
+uart_puts:
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+1:
+  ldrb    w2, [x0], #1
+  cbz     w2, 5f
+  cmp     w2, #127
+  b.ne    4f
+  mov     w2, '('
+2:
+  ldr     w3, [x1, AUX_MU_LSR]                    // w3 = [AUX_MU_LSR_REG]
+  tbz     x3, #5, 2b                              // Repeat last statement until bit 5 is set.
+
+/////////////////////
+// This following section allows us to disable UART output during testing but
+// setting the one byte test system variable 'uart_disable' to a non zero value
+// without affecting any register values so to not impact tests.
+.if       TESTS_INCLUDE
+  adrp    x1, uart_disable
+  add     x1, x1, :lo12:uart_disable
+  ldrb    w1, [x1]
+  cbnz    x1, uart_puts
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+.endif
+/////////////////////
+
+  strb    w2, [x1, AUX_MU_IO_REG]                 //   [AUX_MU_IO_REG] = w2
+  mov     w2, 'c'
+3:
+  ldr     w3, [x1, AUX_MU_LSR]                    // w3 = [AUX_MU_LSR_REG]
+  tbz     x3, #5, 3b                              // Repeat last statement until bit 5 is set.
+  strb    w2, [x1, AUX_MU_IO_REG]                 //   [AUX_MU_IO_REG] = w2
+  mov     w2, ')'
+4:
+  ldr     w3, [x1, AUX_MU_LSR]                    // w3 = [AUX_MU_LSR_REG]
+  tbz     x3, #5, 4b                              // Repeat last statement until bit 5 is set.
+
+/////////////////////
+// This following section allows us to disable UART output during testing but
+// setting the one byte test system variable 'uart_disable' to a non zero value
+// without affecting any register values so to not impact tests.
+.if       TESTS_INCLUDE
+  adrp    x1, uart_disable
+  add     x1, x1, :lo12:uart_disable
+  ldrb    w1, [x1]
+  cbnz    x1, uart_puts
+  adr     x1, mailbox_base                        // x1 = mailbox_base
+  ldr     x1, [x1, aux_base-mailbox_base]         // x1 = [aux_base] = 0x000000003f215000 (rpi3) or 0x00000000fe215000 (rpi4)
+.endif
+/////////////////////
+
+  strb    w2, [x1, AUX_MU_IO_REG]                 //   [AUX_MU_IO_REG] = w2
+  b       1b
+5:
+  ret
+
+
+# ------------------------------------------------------------------------------
+# Write the value of x0 as a hex string (0x0123456789abcdef) to Mini UART.
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   x0 = value to write as a hex string to Mini UART.
+uart_x0:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  stp     x19, x20, [sp, #-16]!                   // Backup x19, x20
+  mov     x19, x0                                 // Backup x0 in x19
+  sub     sp, sp, #0x20                           // Allocate space on stack for hex string
+  mov     w2, 0x7830
+  mov     x1, sp
+  mov     x1, sp
+  strh    w2, [x1], #2                            // "0x"
+  mov     x2, 64
+  bl      hex_x0
+  strb    wzr, [x1], #1
+  mov     x0, sp
+  bl      uart_puts
+  add     sp, sp, #0x20
+  mov     x0, x19                                 // Restore x0
+  ldp     x19, x20, [sp], #16                     // Restore x19, x20
+  ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
+  ret
+.endif

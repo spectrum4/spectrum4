c7dfb5cf16018e0546a553730ef1c23061c59353
Pete Moore <pmoore@mozilla.com>
2022-11-06 14:18:35 +0100

    Upgrade tools in docker container and clean up bootstrap scripts

--- dev-setup/ubuntu/bootstrap-ubuntu.sh
+++ dev-setup/ubuntu/bootstrap-ubuntu.sh
@@ -66,120 +66,130 @@ retry apt-get update
 retry apt-get upgrade -y
 
 # bsdmainutils contains hexdump
-# build-essential is needed for building z80 binutils and libspectrum/fuse
-# libaudiofile-dev is required by tape2wav in fuse-utils
-# libglib2.0 is needed for building libspectrum
+# build-essential is needed for building z80 binutils, aarch64 binutils and fuse
+# fuse-emulator-utils contains tape2wav
+# fuse3 is needed by tup
+# libfuse3-dev is needed by fuse
+# libglib2.0 is needed for building fuse-1.5.7 (seems to fix a pkg-config issue, might be overkill)
 # libgmp-dev is required to build aarch64-none-elf-gdb
 # libncurses-dev might be useful for building aarch64-none-elf-gdb (not sure)
-# libpixman-1-dev and meson needed for building qemu
+# libpixman-1-dev is needed for building qemu
+# libspectrum-dev is needed for building fuse-1.5.7
+# meson is needed for building qemu
+# texinfo is needed for building z80 binutils and aarch64 binutils
+# unzip is needed for unzipping tup
+# wget is needed for downloading curl
 # xz-utils is needed by tar commands below
-retry apt-get install -y bsdmainutils build-essential fuse3 libaudiofile-dev libfuse3-dev libglib2.0 libgmp-dev libncurses-dev libpixman-1-dev meson texinfo unzip wget xz-utils
+retry apt-get install -y bsdmainutils build-essential fuse-emulator-utils fuse3 libfuse3-dev libglib2.0 libgmp-dev libncurses-dev libpixman-1-dev libspectrum-dev meson texinfo unzip wget xz-utils
 
-retry wget -O /usr/local/bin/curl "https://github.com/moparisthebest/static-curl/releases/download/v7.84.0/curl-${ARCH2}"
-chmod a+x /usr/local/bin/curl
+if ! hash curl 2> /dev/null; then
+  retry wget -O /usr/local/bin/curl "https://github.com/moparisthebest/static-curl/releases/download/v7.84.0/curl-${ARCH2}"
+  chmod a+x /usr/local/bin/curl
+fi
 
-# install libspectrum (needed by fuse)
-# export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
-# Note, libspectrum-dev places library files in /usr/lib/x86_64-linux-gnu:
-#   /usr/lib/x86_64-linux-gnu/libspectrum.a
-#   /usr/lib/x86_64-linux-gnu/libspectrum.so.8.8.14
-# retry curl -L 'https://sourceforge.net/projects/fuse-emulator/files/libspectrum/1.4.4/libspectrum-1.4.4.tar.gz/download' > libspectrum-1.4.4.tar.gz
-# tar xvfz libspectrum-1.4.4.tar.gz
-# cd libspectrum-1.4.4
-# ./configure
-# make
-# make install
-# cd ..
-retry apt-get install -y libspectrum-dev
+if ! hash fuse 2> /dev/null; then
 
-# install (headless) fuse which includes ROMs
-retry curl -f -L 'https://sourceforge.net/projects/fuse-emulator/files/fuse/1.5.7/fuse-1.5.7.tar.gz/download' > fuse-1.5.7.tar.gz
-tar xvfz fuse-1.5.7.tar.gz
-cd fuse-1.5.7
-./configure --with-null-ui
-make
-make install
-cd ..
+  # Install (headless) fuse emulator which includes ROMs.
+  # Note, we build from source since we need to specify --with-null-ui so that
+  # windows aren't displayed when unit tests run.
 
-# install fuse-utils to get tape2wav
-retry apt-get install -y fuse-emulator-utils
-# retry curl -f -L 'https://sourceforge.net/projects/fuse-emulator/files/fuse-utils/1.4.3/fuse-utils-1.4.3.tar.gz/download' > fuse-utils-1.4.3.tar.gz
-# tar xvfz fuse-utils-1.4.3.tar.gz
-# cd fuse-utils-1.4.3
-# ./configure
-# make
-# make install
-# cd ..
+  retry curl -fsSL 'https://sourceforge.net/projects/fuse-emulator/files/fuse/1.5.7/fuse-1.5.7.tar.gz/download' > fuse-1.5.7.tar.gz
+  tar xvfz fuse-1.5.7.tar.gz
+  cd fuse-1.5.7
+  ./configure --with-null-ui
+  make -j4
+  make install
+  cd ..
+fi
 
-# install gcc cross-compiler toolchain
-retry curl -f -L "https://developer.arm.com/-/media/Files/downloads/gnu/11.2-2022.02/binrel/gcc-arm-11.2-2022.02-$(uname -m)-aarch64-none-elf.tar.xz" > "gcc-arm-11.2-2022.02-$(uname -m)-aarch64-none-elf.tar.xz"
-tar xvf "gcc-arm-11.2-2022.02-$(uname -m)-aarch64-none-elf.tar.xz"
+# install binutils for targets z80-unknown-elf and aarch64-none-elf
+z80_tools_absent=false
+aarch64_tools_absent=false
+for tool in as ld readelf objcopy objdump; do
+  hash "z80-unknown-elf-${tool}" 2> /dev/null || z80_tools_absent=true
+  hash "aarch64-none-elf-${tool}" 2> /dev/null || aarch64_tools_absent=true
+done
 
-# move gcc tools into /usr/local/bin
-mv "gcc-arm-11.2-2022.02-$(uname -m)-aarch64-none-elf/bin"/* /usr/local/bin
+if ${z80_tools_absent} || ${aarch64_tools_absent}; then
+  retry curl -fsSL 'https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz' > binutils-2.39.tar.gz
+  tar xfz binutils-2.39.tar.gz
 
-# replace arm developer gdb with our own, since theirs seems to depend on
-# libpython3.6m.so.1.0 which isn't available, and is from python 3.6, so pretty
-# old too.
-rm /usr/local/bin/aarch64-none-elf-gdb*
-retry curl -f -L 'https://ftp.gnu.org/gnu/gdb/gdb-12.1.tar.gz' > gdb-12.1.tar.gz
-tar zvfx gdb-12.1.tar.gz
-cd gdb-12.1
-./configure --target=aarch64-none-elf # --disable-werror
-make
-make install
-cd ..
+  if ${z80_tools_absent}; then
+    mkdir binutils-z80-build
+    cd binutils-z80-build
+    ../binutils-2.39/configure \
+      --target=z80-unknown-elf \
+      --disable-werror
+    make -j4
+    make install
+    cd ..
+  fi
 
-# install z80 cross-compiler binutils
-retry curl -f -L 'https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.gz' > binutils.tar.gz
-tar zvfx binutils.tar.gz
-cd binutils-2.39
-./configure --target=z80-unknown-elf --disable-werror
-make
-make install
-cd ..
+  if ${aarch64_tools_absent}; then
+    mkdir binutils-aarch64-build
+    cd binutils-aarch64-build
+    ../binutils-2.39/configure \
+      --target=aarch64-none-elf
+    make -j4
+    make install
+    cd ..
+  fi
+fi
 
-retry curl -f -L 'https://download.qemu-project.org/qemu-5.2.0.tar.xz' > qemu-5.2.0.tar.xz
-tar xvf qemu-5.2.0.tar.xz
-mkdir qemu-5.2.0/build
-cd qemu-5.2.0/build
-../configure --target-list=aarch64-softmmu --disable-vnc
-make -j4
-make install
-cd ../..
+if ! hash aarch64-none-elf-gdb 2> /dev/null; then
+  retry curl -fsSL 'https://ftp.gnu.org/gnu/gdb/gdb-12.1.tar.gz' > gdb-12.1.tar.gz
+  tar zvfx gdb-12.1.tar.gz
+  cd gdb-12.1
+  ./configure --target=aarch64-none-elf
+  make -j4
+  make install
+  cd ..
+fi
 
-retry curl -f -L 'https://github.com/gittup/tup/archive/b037d4b211de6025703b77c3287b76159656ef22.zip' > tup.zip
-unzip tup.zip
-cd tup-*
-# Note, we don't use ./bootstrap.sh here because it requires privileges which
-# are not available during `docker build`. Now I think about it though, maybe
-# we could build tup inside a docker container (using `docker run` with
-# appropriate privileges), and then copy it into the image that we build with a
-# COPY directive in the Dockerfile. For now though, this works and is
-# sufficient.
-CFLAGS="-g" ./build.sh
-mv build/tup /usr/local/bin
-cd ..
-# apt-get install -y software-properties-common
-# retry apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 486D3664
-# apt-add-repository 'deb http://ppa.launchpad.net/anatol/tup/ubuntu precise main'
-# retry apt-get update --allow-insecure-repositories
-# apt-get install tup
+if ! hash qemu-system-aarch64 2> /dev/null; then
+  retry curl -fsSL 'https://download.qemu-project.org/qemu-7.1.0.tar.xz' > qemu-7.1.0.tar.xz
+  tar xvf qemu-7.1.0.tar.xz
+  mkdir qemu-7.1.0/build
+  cd qemu-7.1.0/build
+  ../configure --target-list=aarch64-softmmu --disable-vnc
+  make -j4
+  make install
+  cd ../..
+fi
 
-# install go 1.19
-mkdir -p /usr/lib
-cd /usr/lib
-retry curl -f -L "https://golang.org/dl/go1.19.linux-${ARCH}.tar.gz" > "go1.19.linux-${ARCH}.tar.gz"
-tar xvfz "go1.19.linux-${ARCH}.tar.gz"
-rm "go1.19.linux-${ARCH}.tar.gz"
+if ! hash tup 2> /dev/null; then
+  retry curl -fsSL 'https://github.com/gittup/tup/archive/b037d4b211de6025703b77c3287b76159656ef22.zip' > tup.zip
+  unzip tup.zip
+  cd tup-*
+  # Note, we don't use ./bootstrap.sh here because it requires privileges which
+  # are not available during `docker build`. Now I think about it though, maybe
+  # we could build tup inside a docker container (using `docker run` with
+  # appropriate privileges), and then copy it into the image that we build with a
+  # COPY directive in the Dockerfile. For now though, this works and is
+  # sufficient.
+  CFLAGS="-g" ./build.sh
+  mv build/tup /usr/local/bin
+  cd ..
+fi
 
-# install shfmt
-/usr/lib/go/bin/go install mvdan.cc/sh/v3/cmd/shfmt@latest
-mv "${HOME}/go/bin/shfmt" /usr/local/bin/shfmt
+if ! hash go 2> /dev/null; then
+  # install go 1.19.3
+  mkdir -p /usr/lib
+  cd /usr/lib
+  retry curl -fsSL "https://golang.org/dl/go1.19.3.linux-${ARCH}.tar.gz" > "go1.19.3.linux-${ARCH}.tar.gz"
+  tar xvfz "go1.19.3.linux-${ARCH}.tar.gz"
+  rm "go1.19.3.linux-${ARCH}.tar.gz"
+fi
+
+if ! shfmt 2> /dev/null; then
+  # install shfmt
+  /usr/lib/go/bin/go install mvdan.cc/sh/v3/cmd/shfmt@latest
+  mv "${HOME}/go/bin/shfmt" /usr/local/bin/shfmt
+fi
 
 cd
 echo "Deleting '${PREP_DIR}' ..."
-rm -rf "${PREP_DIR}"
+rm -r "${PREP_DIR}"
 
 echo "Please note the following environment variables need to be set in order for the tools to be available:"
 echo "$ export PATH='/usr/lib/go/bin:/bin:/usr/bin:/usr/local/bin'"

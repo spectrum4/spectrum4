1827a489ba07ab4ba1b4c96c350aacd567d17d91
Pete Moore <pmoore@mozilla.com>
2023-12-30 14:38:15 +0000

    wip

--- src/spectrum4/kernel/pci.s
+++ src/spectrum4/kernel/pci.s
@@ -11,26 +11,36 @@
 #   x0 = bus data address
 #   x1 = devfn
 #   x2 = where (pci reg offset)
-#   x3 = size
+#   w3 = size
 #   w4 = *val
 #
 # On return:
-#   w0 = 0 (success) or 0x86 (PCI device not found)
-#   w1 = value read
+#   w0 = 0x00 (PCI successful), or
+#        0x86 (PCI device not found), or
+#        0x87 (PCI bad register number => unaligned access)
+#   w1 = value read (if access was aligned)
+#   w2 = untouched
+#   x3 disturbed
+#   x4 disturbed
+#   w5 = size - 1
 #
-# Based on https://github.com/raspberrypi/linux/blob/14b35093ca68bf2c81bbc90aace5007142b40b40/drivers/pci/access.c#L77-L96
+# Based on
+#   * https://github.com/raspberrypi/linux/blob/3ea9b35bd3eef731a94c48e197d4ea1539f3aae2/drivers/pci/access.c#L35-L48
+#   * https://github.com/raspberrypi/linux/blob/14b35093ca68bf2c81bbc90aace5007142b40b40/drivers/pci/access.c#L77-L96
 pci_read:
+  sub     w5, w3, #1
+  tst     w2, w5
+  b.ne    6f
   stp     x29, x30, [sp, #-16]!
   mov     x29, sp
   bl      map_conf
   cbnz    x0, 1f
   mov     w1, #-1
-  mov     w0, #0x86
+  mov     w0, #0x86                               // PCIBIOS_DEVICE_NOT_FOUND
   b       5f
 1:
-  cmp     w3, #0x1
-  b.eq    3f
-  cmp     w3, #0x2
+  cbz     w5, 3f
+  cmp     w5, #0x1
   b.eq    2f
   ldr     w1, [x0]
   b       4b
@@ -41,8 +51,11 @@ pci_read:
   ldrb    w1, [x0]
 4:
   dmb     oshld
-  mov     w0, #0
+  mov     w0, #0                                  // PCIBIOS_SUCCESSFUL
 5:
   str     w1, [x4]
   ldp     x29, x30, [sp], #16
+  ret
+6:
+  mov     w0, #0x87                               // PCIBIOS_BAD_REGISTER_NUMBER (unaligned access)
   ret

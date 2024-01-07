b98eec3d52530763f5b880f89cb1dcdac76a43cd
Pete Moore <pmoore@mozilla.com>
2024-01-07 15:54:23 +0100

    Pass --init to docker run (tup) command

--- tup-under-docker.sh
+++ tup-under-docker.sh
@@ -7,8 +7,10 @@
 #####################################################################################
 
 # This script runs tup under docker to build and test everything.
-# To see how the docker image was created, see the docker subdirectory.
+# To see how the docker image was created, see the dev-setup/docker subdirectory.
 
 cd "$(dirname "${0}")"
 TAG="$(cat dev-setup/docker/TAG)"
-docker run -t --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined --rm -v "$(pwd):/spectrum4" -w /spectrum4 "${TAG}" tup "${@}"
+# use --init to capture signals correctly (e.g. Ctrl-C)
+# see https://ddanilov.me/how-signals-are-handled-in-a-docker-container
+docker run --init -t --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined --rm -v "$(pwd):/spectrum4" -w /spectrum4 "${TAG}" tup "${@}"

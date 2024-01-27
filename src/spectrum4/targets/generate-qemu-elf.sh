cadbe305cd9de51e1cb79c4ee890e2e25f7afd49
Pete Moore <pmoore@mozilla.com>
2024-01-27 15:35:55 +0100

    wip

--- src/spectrum4/targets/generate-qemu-elf.sh
+++ src/spectrum4/targets/generate-qemu-elf.sh
@@ -15,8 +15,21 @@ o_input_file="${2}"
 
 shift 2
 
-# Link the .o file to generate the elf file
-"${@}" -o "${elf_output_file}" "${o_input_file}" ../kernel/*.o
+# This horrible hack is needed because --as-needed won't work for us currently
+# since pci code depends on spectrum4 (for uart logging), and spectrum4 depends
+# on pci (for pci host probe). The way to fix this would be to split uart into
+# its own module that spectrum4 and pci both depend on, but this is just an
+# experimental branch, so not really worth it, as the C code isn't here to
+# stay, just to get things working before converting just the parts we need to
+# assembly. Note, we take the trouble to avoid linking in pci code if not
+# needed, so all tests don't run when we change pci code that they don't use!
+: ${AARCH64_TOOLCHAIN_PREFIX:=aarch64-none-elf-}
+if [ "$("${AARCH64_TOOLCHAIN_PREFIX}readelf" -s "${o_input_file}" | sed -n 's/^  *[^ ][^ ]*  *[^ ][^ ]*\(1\).* ABS PCI_INCLUDE$/\1/p')" == '1' ]; then
+  "${@}" -o "${elf_output_file}" "${o_input_file}" ../kernel/*.o
+else
+  "${@}" -o "${elf_output_file}" "${o_input_file}"
+fi
+
 # Now patch the elf program header to set p_paddr to physical address
 # 0x0000000000000000 instead of 0xffff000000000000. See:
 #
@@ -29,8 +42,8 @@ shift 2
 # linked with virtual addresses as required, but to trick qemu into loading the
 # text image from the elf file at physical address 0x0. This emulates the
 # behaviour of kernel_old=1 in the config.txt file that qemu doesn't use.
-# Otherwise, if we specify the .img file in the qemu command, we have no way
-# of specifying a load address, and it will use 0x80000 which we don't want.
-# If we didn't patch the physical address, qemu would try to load the code
-# at the virtual address 0xffff000000000000, which wouldn't work.
+# Otherwise, if we specify the .img file in the qemu command, we have no way of
+# specifying a load address, and it will use 0x80000 which we don't want.  If
+# we didn't patch the physical address, qemu would try to load the code at the
+# virtual address 0xffff000000000000, which wouldn't work.
 printf '\x0\x0\' | dd of="${elf_output_file}" bs=1 seek=94 count=2 conv=notrunc 2> /dev/null

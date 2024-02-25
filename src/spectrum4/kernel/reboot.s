be35173d85ca750fd18ef57bfec203424e353c90
Pete Moore <pmoore@mozilla.com>
2024-02-25 09:07:26 +0100

    Remove more peripheral register indirection

--- src/spectrum4/kernel/reboot.s
+++ src/spectrum4/kernel/reboot.s
@@ -2,14 +2,6 @@
 # Licencing information can be found in the LICENCE file
 # (C) 2021 Spectrum +4 Authors. All rights reserved.
 
-# .set PM_BASE,                    0x3f100000
-# .set PM_WDOG,                    0x24
-# .set PM_RSTC,                    0x1c
-# .set PM_RSTC_WRCFG_CLR,          0xffffffcf
-# .set PM_RSTC_WRCFG_FULL_RESET,   0x00000020
-# .set PM_PASSWORD,                0x5a000000
-#
-#
 # # ------------------------------------------------------------------------------
 # # Reboots the machine after ~150us
 # # See:
@@ -26,19 +18,19 @@
 #   mov     x4, #0x00ff0000
 #   bl      paint_string
 # .endif
-#   mov     x0, 0x8000000
+#   mov     x0, #0x8000000
 #   bl      wait_cycles
 #   adr     x0, mbox_reboot
 #   bl      mbox_call
-#   mov     x0, PM_BASE
-#   mov     w1, PM_PASSWORD
-#   orr     w2, w1, #0x0c
-#   str     w2, [x0, PM_WDOG]                       // [0x3f100024] = 0x5a00000a
-#   ldr     w3, [x0, PM_RSTC]
-#   and     w3, w3, PM_RSTC_WRCFG_CLR
-#   orr     w3, w3, w1
-#   orr     w3, w3, PM_RSTC_WRCFG_FULL_RESET
-#   str     w3, [x0, PM_RSTC]                       // [0x3f10001c] = ([0x3f10001c] & 0xffffffcf) | 0x5a000020
+#   adrp    x0, #0x3f100000                         // x0 = PM_BASE
+#   mov     w1, #0x5a000000                         // w1 = PM_PASSWORD
+#   orr     w2, w1, 0x0a                            // w2 = PM_PASSWORD | 10 (=> 10 ticks ~150us)
+#   str     w2, [x0, #0x24]                         // [PM_WDOG] = [0x3f100024] = 0x5a00000a
+#   ldr     w3, [x0, #0x1c]                         // w3 = [PM_RSTC] = [0x3f10001c]
+#   and     w3, w3, #0xffffffcf                     // w3 &= PM_RSTC_WRCFG_CLR (clear bits 4, 5)
+#   orr     w3, w3, w1                              // w3 |= 0x5a000000 (PM_PASSWORD)
+#   orr     w3, w3, #0x00000020                     // set bit 5 (PM_RSTC_WRCFG_FULL_RESET)
+#   str     w3, [x0, #0x1c]                         // [0x3f10001c] = ([0x3f10001c] & 0xffffffcf) | 0x5a000020
 # # msr     daifset, #2
 #   b       sleep
 #

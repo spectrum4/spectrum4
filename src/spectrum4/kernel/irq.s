062dea65931b44e0cf2e15405eff2b6a25d51620
Pete Moore <pmoore@mozilla.com>
2025-07-06 16:17:33 +0200

    Switch from legacy interrupt controller to GIC-400 on rpi400 and switch from BCM system timer to ARM generic timer (CNTP) on both rpi3b and rpi400

--- src/spectrum4/kernel/irq.s
+++ src/spectrum4/kernel/irq.s
@@ -54,35 +54,61 @@ show_invalid_entry_message:
 
 
 enable_ic_bcm283x:
-  mov     w0, #0x00000002
-  adrp    x1, 0x3f00b000 + _start
-  str     w0, [x1, #0x210]                        // [0x3f00b210] = 0x00000002
+  adrp    x0, 0x40000000 + _start
+  ldr     w1, [x0, #0x40]                         // w1 = [ARM_LOCAL register TIMER_CNTRL0] = [0x40000040]
+  orr     w1, w1, 0x2                             // set bit 1 (CNT_PNS_IRQ) to cause system timer to raise an IRQ to core 0
+  str     w1, [x0, #0x40]
   ret
 
 
 enable_ic_bcm2711:
-  mov     w0, #0x00000002
-  adrp    x1, 0xfe00b000 + _start
-  str     w0, [x1, #0x210]                        // [0xfe00b210] = 0x00000002
+  adrp    x1, 0xff841000 + _start                 // x1 = GICD base address
+  str     wzr, [x1]                               // [0xff841000]     = [GICD_CTLR]        = 0x00000000                                             => disable GIC distributor
+  mov     w3, #0x8
+  mov     w4, #0xffffffff
+  add     x5, x1, #0x180
+  1:
+    str     w4, [x5, #0x200]                      // [0xff841380+4*n] = [GICD_ICACTIVERn]  = 0xffffffff for n=0 to 7 (1 bit per interrupt)          => clear all 256 active interrupts
+    str     w4, [x5, #0x100]                      // [0xff841280+4*n] = [GICD_ICPENDRn]    = 0xffffffff for n=0 to 7 (1 bit per interrupt)          => clear all 256 pending interrupts
+    str     w4, [x5], #0x4                        // [0xff841180+4*n] = [GICD_ICENABLERn]  = 0xffffffff for n=0 to 7 (1 bit per interrupt)          => disable all 256 interrupts
+    subs    w3, w3, #1
+    b.ne    1b
+  mov     w3, #0x40
+  mov     w6, #0x01010101
+  movl    w7, 0xa0a0a0a0
+  add     x5, x1, #0x400
+  2:
+    str     w6, [x5, #0x400]                      // [0xff841800+4*n] = [GICD_ITARGETSRn]  = 0x01010101 for n=0 to 63 (0x3f) (8 bits per interrupt) => route all 256 interrupts to core 0
+    str     w7, [x5], #0x4                        // [0xff841400+4*n] = [GICD_IPRIORITYRn] = 0xa0a0a0a0 for n=0 to 63 (0x3f) (8 bits per interrupt) => set all 256 interrupts priority to 0xa0
+    subs    w3, w3, #1
+    b.ne    2b
+  mov     w3, #0x10
+  mov     w8, #0x55555555
+  add     x5, x1, #0xc00
+  3:
+    str     w8, [x5], #0x4                        // [0xff841c00+4*n] = [GICD_ICFGRn]      = 0x55555555 for n=0 to 15 (0xf) (2 bits per interrupt) => set all 256 interrupts to level-sensitive
+    subs    w3, w3, #1
+    b.ne    3b
+  mov     w4, #0x1
+  str     w4, [x1]                                // [0xff841000]     = [GICD_CTLR]        = 0x00000001                                            => forward group 1 interrupts from GIC distributor
+  mov     w5, #0xf0
+  str     w5, [x1, #0x1004]                       // [0xff842004]     = [GICC_PMR]         = 0x000000f0                                            => priority mask = 0xf0
+  mov     w6, #0x261
+  str     w6, [x1, #0x1000]                       // [0xff842000]     = [GICC_CTLR]        = 0x00000261                                            => EOImodeNS: 1, IRQBypDisGrp1: 1, FIQBypDisGrp1: 1, EnableGrp1: 1
+  mov     w4, #0x40000000
+  str     w4, [x1, #0x100]                        // [0xff841100]     = [GICD_ISENABLER0]  = 0x40000000                                            => enable interrupt 30 (0x1e)
   ret
 
 
 handle_irq_bcm283x:
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  adrp    x1, 0x3f00b000 + _start
-  ldr     w0, [x1, #0x204]                        // w0 = [0x3f00b204]
-  cmp     w0, #2
-  b.ne    1f
+  adrp    x1, 0x40000000 + _start
+  ldr     w7, [x1, #0x60]                         // w7 = [0x40000060] = "Core0 IRQ Source" register
+                                                  //   https://datasheets.raspberrypi.com/bcm2836/bcm2836-peripherals.pdf (page 7)
+  cmp     w7, #0x2                                // test bit 1 (CNTPNSIRQ, page 16)
+  b.ne    panic_unknown_interrupt
   bl      handle_timer_irq
-.if UART_DEBUG
-  b       2f
-.endif
-1:
-.if UART_DEBUG
-  bl      log_unknown_interrupt_value
-2:
-.endif
   ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
   ret
 
@@ -90,36 +116,37 @@ handle_irq_bcm283x:
 handle_irq_bcm2711:
   stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
   mov     x29, sp                                 // Update frame pointer to new stack location.
-  adrp    x1, 0xfe00b000 + _start
-  ldr     w0, [x1, #0x200]                        // w0 = [0xfe00b200]
-  cmp     w0, #2
-  b.ne    1f
+  adrp    x8, 0xff842000 + _start
+  ldr     w7, [x8, #0xc]                          // w7 = [0xff84200c] = [GICC_IAR]
+  cmp     w7, #0x1e
+  b.ne    panic_unknown_interrupt
+  str     w7, [x8, #0x10]                         // [0xff842010] = [GICC_EOIR] = [GICC_IAR]
+                                                  // Note: Writing to GICC_EOIR before servicing interrupt, which I believe means the
+                                                  // interrupt routine will be reentrant at this point. Writing to EOIR after
+                                                  // handling timer may be safer.
+  dsb     sy                                      // The GIC architecture specification requires that valid EOIR writes are ordered
+                                                  // however probably not needed since device memory writes should already be ordered.
   bl      handle_timer_irq
-.if UART_DEBUG
-  b       2f
-.endif
-1:
-.if UART_DEBUG
-  bl      log_unknown_interrupt_value
-2:
-.endif
+  str     w7, [x8, #0x1000]                       // [0xff843000] = [GICC_DIR]  = [GICC_IAR]
+                                                  // Note: Could set GICC_CTLR.EOImodeNS to 0 and not have separate GICC_EOIR and
+                                                  // GICC_DIR writes, i.e. just write to GICC_EOIR after servicing interrupt.
   ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
   ret
 
 
+panic_unknown_interrupt:
 .if UART_DEBUG
-log_unknown_interrupt_value:
-  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
-  mov     x29, sp                                 // Update frame pointer to new stack location.
+  mov     x0, x7
   adr     x1, msg_unknown_interrupt_value
   mov     x2, #32
   bl      hex_x0
   adr     x0, msg_unknown_interrupt
   bl      uart_puts
-  ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
-  ret
+.endif
+  b       sleep
 
 
+.if UART_DEBUG
 msg_unknown_interrupt:
   .ascii "Unknown interrupt: 0x"                  // concatenates with the string below
 msg_unknown_interrupt_value:

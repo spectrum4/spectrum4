927375b315630942ddf1dd91c454fa28781e90d5
Pete Moore <pmoore@mozilla.com>
2025-08-23 12:08:38 +0200

    wip

--- src/spectrum4/kernel/xhci.s
+++ src/spectrum4/kernel/xhci.s
@@ -5,6 +5,7 @@
 .text
 
 
+# Note: preserve x7 and x8 since caller (handle_irq_bcm2711) uses these
 # On exit:
 #   x1: new timer value ([next_interrupt])
 #   x2: 0x2000000
@@ -17,15 +18,41 @@ handle_xhci_irq:
   adr     x0, msg_xhci_event
   bl      uart_puts
 .endif
-  adrp    x7, 0xfd504000 + _start                 // x4 = MSI status register
-  ldr     w0, [x7, #0x500]                        // w0 = [MSI status]
+  adrp    x9, 0xfd504000 + _start                 // x4 = MSI status register
+  ldr     w0, [x9, #0x500]                        // w0 = [MSI status]
 .if UART_DEBUG
   bl      uart_x0                                 // log MSI interrupt vectors
   bl      uart_newline
 .endif
-  str     w0, [x7, #0x508]                        // clear MSI interrupts
+  bl      consume_xhci_events
+  str     w0, [x9, #0x508]                        // clear MSI interrupts
   ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
   ret
+
+.align 2
+# Note preserve x7, x8 and x9 since call stack (handle_xhci_irq and handle_irq_bcm2711) use these
+consume_xhci_events:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  adr     x12, xhci_vars
+  ldr     x10, [x12, xhci_event_dequeue-xhci_vars]
+  1:
+    ldp     x0, x11, [x10], #16
+    tbz     x11, #32, 2f
+.if UART_DEBUG
+    bl      uart_x0
+    bl      uart_newline
+    mov     x0, x11
+    bl      uart_x0
+    bl      uart_newline
+.endif
+    b       1b
+2:
+  str     x10, [x12, xhci_event_dequeue-xhci_vars]
+                                                  // advance event dequeue pointer
+  ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
+  ret
+
 
 
 .if UART_DEBUG

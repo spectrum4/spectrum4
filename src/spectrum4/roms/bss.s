28d5e464e433a205df429147aade1cf17a222084
Pete Moore <pmoore@mozilla.com>
2025-08-24 11:14:13 +0200

    Loop to start of event ring when EDRP advances past end

--- src/spectrum4/roms/bss.s
+++ src/spectrum4/roms/bss.s
@@ -400,6 +400,8 @@ command_ring:    .space 0x100 * 0x10              // 256 entries
 
 .align 12
 event_ring:      .space 0xfc * 0x10               // 252 entries (so that ERST can fit in same page)
+event_ring_end:
+
 .align 6                                          // Needs to be 64 byte aligned since ERSTBA bits 0-5 are RsvdP
 erst:            .space 0x40                      // only the first 0x10 bytes matter, but reserve 0x40 since spec says:
                                                   // The Ring Segment Size may be set to any value from 16 to 4096, however
